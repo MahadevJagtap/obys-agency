be9b681239b16320ca4ea8baaf8b88167208f0dd
imahadxv <mmjagtap007@gmail.com>
2024-09-01 20:20:20 +0530

    page 01 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function loadingAnimation(){
+  var tl = gsap.timeline()
 tl.from('.line h1',{
   y:150,
   stagger:0.2,
@@ -26,7 +27,7 @@ animationName:'anime'
 tl.to('.loader',{
   opacity:0,
   duration:0.3,
-  delay:4,
+  delay:3.6,
 })
 tl.from('.page1',{
   y:1600,
@@ -38,3 +39,24 @@ tl.from('.page1',{
 tl.to('.loader',{
   display:'none'
 })
+tl.from('#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1',{
+  y:100,
+  stagger:0.2
+})
+tl.from('.nav-part1',{
+  opacity:0,
+  y:-100,
+})
+}
+function curserAnimation(){
+  document.addEventListener('mousemove',function(dets){
+    gsap.to('.curser',{
+      left:dets.x,
+      top:dets.y
+    })
+  });
+  Shery.makeMagnet('.nav-part1 h3')
+}
+loadingAnimation()
+
+curserAnimation()
