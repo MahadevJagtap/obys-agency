0d8951e7bad0936d8c554c5807a9fd92a0715b0d
imahadxv <mmjagtap007@gmail.com>
2024-09-03 00:08:48 +0530

    page-06 and locomotive done

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function locomotive(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
 function loadingAnimation(){
   var tl = gsap.timeline()
 tl.from('.line h1',{
@@ -27,7 +59,7 @@ animationName:'anime'
 tl.to('.loader',{
   opacity:0,
   duration:0.3,
-  delay:3.6,
+  delay:0,
 })
 tl.from('.page1',{
   y:1600,
@@ -43,6 +75,14 @@ tl.from('#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1',{
   y:140,
   stagger:0.2
 })
+tl.from(
+  "#hero1",
+  {
+    opacity: 0,
+  },
+  "-=1.2"
+);
+
 tl.from('.nav-part1',{
   opacity:0,
   y:-100,
@@ -60,3 +100,5 @@ function curserAnimation(){
 loadingAnimation()
 
 curserAnimation()
+
+locomotive()
